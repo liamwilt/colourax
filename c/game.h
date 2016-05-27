50e4498bcda6291434b5734b2757fbf9905c0756
liamwilt <liam1wilt@gmail.com>
2016-05-26 22:43:30 -0400

    Initial header files with Struct for circle drawings within main().

---
+++ c/game.h
@@ -0,0 +1,94 @@
+static void error(int error, const char *desc)
+{
+        fputs(desc, stderr);
+}
+
+static void key_callback(GLFWwindow *w, int key, int scancode, int action, int mods)
+{
+        if ((key == GLFW_KEY_ESCAPE || key == GLFW_KEY_Q) && action == GLFW_PRESS)
+                glfwSetWindowShouldClose(w, GL_TRUE);
+                
+        switch (key)
+        {
+            case GLFW_KEY_A:
+                if (action == GLFW_PRESS)
+                    printf("A_Key Pressed.\n");
+                    break;
+            case GLFW_KEY_L:
+                if (action == GLFW_PRESS)
+                    printf("L_Key Pressed.\n");
+                    break;
+        }
+}
+
+void drawCircle(float cx, float cy, float radius) 
+{ 
+    float num_segments = 360;
+	float theta = 2 * 3.1415926 / num_segments; 
+	float c = cosf(theta);//precalculate the sine and cosine
+	float s = sinf(theta);
+	float t;
+
+	float x = radius;//we start at angle = 0 
+	float y = 0; 
+    
+	glBegin(GL_TRIANGLE_FAN);
+	glColor3f(1, 0, 1); 
+	for(int ii = 0; ii < num_segments; ii++) 
+	{ 
+		glVertex2f(x + cx, y + cy);//output vertex 
+        
+		//apply the rotation matrix
+		t = x;
+		x = c * x - s * y;
+		y = s * t + c * y;
+	} 
+	glEnd();
+}
+
+void drawTarget(float cx, float cy, float radius) 
+{ 
+    float num_segments = 360;
+	float theta = 2 * 3.1415926 / num_segments; 
+	float c = cosf(theta);//precalculate the sine and cosine
+	float s = sinf(theta);
+	float t;
+
+	float x = radius;//we start at angle = 0 
+	float y = 0; 
+    
+	glBegin(GL_TRIANGLE_FAN);
+	glColor4f(1, 1, 1, 0.50); 
+	for(int ii = 0; ii < num_segments; ii++) 
+	{ 
+		glVertex2f(x + cx, y + cy);//output vertex 
+        
+		//apply the rotation matrix
+		t = x;
+		x = c * x - s * y;
+		y = s * t + c * y;
+	} 
+	glEnd(); 
+}
+
+void drawTrack01()
+{
+    glLineWidth(1.0);
+    glColor3f(1.0,1.0,1.0);
+    glBegin(GL_LINES);
+    glVertex3f(1280 * 0.25,0.0,0.0);
+    glVertex3f(1280 * 0.25,720 * 0.90,0.0);
+    glEnd();
+}
+
+void drawTrack02()
+{
+    glLineWidth(1.0);
+    glColor3f(1.0,1.0,1.0);
+    glBegin(GL_LINES);
+    glVertex3f(1280 * 0.75,0.0,0.0);
+    glVertex3f(1280 * 0.75,720 * 0.90,0.0);
+    glEnd();
+}
+
+
